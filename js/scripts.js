3427b3d1a783f77c146fdb7798a04056456a1f2c
Ahana <ahanasi@seas.upenn.edu>
2020-03-17 14:55:59 -0700

    Update grid styles
    If applied, this commit applies a fixed height and width to the
    grid container. The number of rows and columns in the grid are dynamically
    generated and are styled to make equal use of free space in the grid.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,11 @@
 const gridContainer = document.querySelector(".grid-container");
 let i = 0;
-while (i<16){
+let gridSize = 10;
+document.documentElement.style.setProperty("--colNum", gridSize);
+document.documentElement.style.setProperty("--rowNum", gridSize);
+
+
+while (i<Math.pow(gridSize,2)){
     const grid = document.createElement('div');
     grid.className = "grid-item";
     gridContainer.append(grid);
