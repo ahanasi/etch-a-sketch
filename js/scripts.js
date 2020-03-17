0861d07b05165f9e38734b8b94aa73fe87738eda
Ahana <ahanasi@seas.upenn.edu>
2020-03-17 15:43:44 -0700

    Wrap grid-container in main-content div

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,18 @@
 const gridContainer = document.querySelector(".grid-container");
-let i = 0;
+const boxes = document.getElementById("sketchpad").childNodes;
+
 let gridSize = 10;
 document.documentElement.style.setProperty("--colNum", gridSize);
 document.documentElement.style.setProperty("--rowNum", gridSize);
 
 
+let i = 0;
 while (i<Math.pow(gridSize,2)){
     const grid = document.createElement('div');
     grid.className = "grid-item";
     gridContainer.append(grid);
     i++; 
 }
-
-const boxes = document.querySelectorAll(".grid-item");
 
 boxes.forEach(box => {
     box.addEventListener("mouseleave", (e) => {
@@ -21,4 +21,14 @@ boxes.forEach(box => {
     box.addEventListener("mouseover", (e) => {
         e.target.style.backgroundColor = "pink";
     });
+    
 });
+
+
+
+
+    
+
+
+
+
