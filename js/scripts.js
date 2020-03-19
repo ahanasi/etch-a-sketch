542463845dc7f7747f0d07c5c7eae27d18419a12
Ahana <ahanasi@seas.upenn.edu>
2020-03-18 18:24:51 -0700

    Create common functions
    If applied, this commit creates three functions - createGrid, colorGrid
    and clearGrid.

--- js/scripts.js
+++ js/scripts.js
@@ -2,46 +2,35 @@ const gridContainer = document.querySelector(".grid-container");
 const boxes = document.getElementById("sketchpad").childNodes;
 const reset = document.getElementById("reset");
 
-
 let gridSize = 10;
-document.documentElement.style.setProperty("--colNum", gridSize);
-document.documentElement.style.setProperty("--rowNum", gridSize);
 
+createGrid();
+colorGrid();
 
-let i = 0;
-while (i<Math.pow(gridSize,2)){
-    const grid = document.createElement('div');
-    grid.className = "grid-item";
-    gridContainer.append(grid);
-    i++; 
-}
-
-boxes.forEach(box => {
-    box.addEventListener("mouseleave", (e) => {
-        e.target.style.backgroundColor = "pink";
-    });
-    box.addEventListener("mouseover", (e) => {
-        e.target.style.backgroundColor = "pink";
-    });
+reset.addEventListener('click', () => {
     
-});
-
-reset.addEventListener('click', (e) => {
-    while (gridContainer.lastElementChild) {
-        gridContainer.removeChild(gridContainer.lastElementChild);
-      }
-
+    clearGrid();
     gridSize = +prompt("How many squares per side for the new grid?", "16");
+    createGrid();
+    colorGrid();
+})
+
+function createGrid(){
+
     document.documentElement.style.setProperty("--colNum", gridSize);
     document.documentElement.style.setProperty("--rowNum", gridSize);
 
     let i = 0;
     while (i<Math.pow(gridSize,2)){
-    const grid = document.createElement('div');
-    grid.className = "grid-item";
-    gridContainer.append(grid);
-    i++; 
+        const grid = document.createElement('div');
+        grid.className = "grid-item";
+        gridContainer.append(grid);
+        i++; 
+    }
 
+}
+
+function colorGrid(){
     boxes.forEach(box => {
         box.addEventListener("mouseleave", (e) => {
             e.target.style.backgroundColor = "pink";
@@ -49,11 +38,19 @@ reset.addEventListener('click', (e) => {
         box.addEventListener("mouseover", (e) => {
             e.target.style.backgroundColor = "pink";
         });
-        
     });
 }
-    
-})
+
+function clearGrid(){
+    while (gridContainer.lastElementChild) {
+        gridContainer.removeChild(gridContainer.lastElementChild);
+      }
+}
+
+
+
+
+
 
 
 
