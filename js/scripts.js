829b2c0028c41310fe4b8fc2de9175aba55a56b3
Ahana <ahanasi@seas.upenn.edu>
2020-03-18 18:10:13 -0700

    Clear grid and prompt user for grid size on reset

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 const gridContainer = document.querySelector(".grid-container");
 const boxes = document.getElementById("sketchpad").childNodes;
+const reset = document.getElementById("reset");
+
 
 let gridSize = 10;
 document.documentElement.style.setProperty("--colNum", gridSize);
@@ -23,6 +25,39 @@ boxes.forEach(box => {
     });
     
 });
+
+reset.addEventListener('click', (e) => {
+    while (gridContainer.lastElementChild) {
+        gridContainer.removeChild(gridContainer.lastElementChild);
+      }
+
+    gridSize = +prompt("How many squares per side for the new grid?", "16");
+    document.documentElement.style.setProperty("--colNum", gridSize);
+    document.documentElement.style.setProperty("--rowNum", gridSize);
+
+    let i = 0;
+    while (i<Math.pow(gridSize,2)){
+    const grid = document.createElement('div');
+    grid.className = "grid-item";
+    gridContainer.append(grid);
+    i++; 
+
+    boxes.forEach(box => {
+        box.addEventListener("mouseleave", (e) => {
+            e.target.style.backgroundColor = "pink";
+        });
+        box.addEventListener("mouseover", (e) => {
+            e.target.style.backgroundColor = "pink";
+        });
+        
+    });
+}
+    
+})
+
+
+
+
 
 
 
